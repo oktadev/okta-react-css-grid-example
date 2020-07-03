4babcc70165f7573994f28cba567e444f1d2ceba
Holger Schmitz <holger@notjustphysics.com>
2020-07-03 15:29:26 +0100

    Creating the application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Unicode from './Unicode';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          Unicode Chart
+        </header>
+        <div className="page">
+          <div className="content">
+              <Unicode />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Unicode.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import unicode from './unicode/unicode';
+import blocks from './unicode/blocks';
+
+class Unicode extends React.Component {
+  constructor(props) {
+    super();
+    this.state = { results: this.search('') };
+  }
+
+  search(query) {
+    return unicode
+      .filter((entry) => entry.name.toUpperCase().includes(query.toUpperCase()))
+      .slice(0, 100);
+  }
+
+  selectChar(entry) {
+    const codeInt = parseInt(entry.code, 16);
+    const block = this.findBlock(codeInt);
+    this.setState({
+      selected: {
+        ...entry,
+        html: String.fromCharCode(codeInt),
+        block: block && block.name
+      }
+    });
+  }
+
+  findBlock(code) {
+    return blocks.filter((b) => (b.from<=code && b.to>=code))[0];
+  }
+
+  render() {
+    const handleSelect = (entry) => () => {
+      this.selectChar(entry);
+    };
+
+    const handleSearch = (event) => {
+      this.setState({
+        results: this.search(event.target.value)
+      });
+    };
+    const resultList = this.state.results.map((entry) =>
+      <li key={entry.code}
+          onClick={handleSelect(entry)}
+          className={(this.state.selected && entry.code===this.state.selected.code) ? 'active' : ''}>
+          {entry.name}
+      </li>
+    );
+    const charInfo = this.state.selected
+      ? <div>
+          <p className="char-name">{ this.state.selected.name }</p>
+          <p className="char-block">{ this.state.selected.block }</p>
+          <p className="char-code">&#x{ this.state.selected.code };</p>
+        </div>
+      : undefined;
+
+    return (
+      <div className="app-ui">
+        <div className="search-input" onChange={handleSearch}><input type="text" placeholder="Search"/></div>
+        <div className="search-results">
+          <ul>{resultList}</ul>
+        </div>
+        <div className="char-display">{ this.state.selected && this.state.selected.html }</div>
+        <div className="char-info">{ charInfo }</div>
+      </div>
+    );
+  }
+}
+
+export default Unicode;
